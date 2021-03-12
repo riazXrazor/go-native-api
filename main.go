896acde5563111ee081c370c63df2060961043f9
Riaz Laskar <riazcool77@gmail.com>
2021-03-12 14:08:02 +0530

    initial

--- main.go
+++ main.go
@@ -14,19 +14,19 @@ func (s *server) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 		case "GET":
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "get req !!}`))
+			w.Write([]byte(`{"message": "get req !!"}`))
 		case "POST":
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "post req!!}`))
+			w.Write([]byte(`{"message": "post req!!"}`))
 		case "PUT":
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "put req!!}`))
+			w.Write([]byte(`{"message": "put req!!"}`))
 		case "DELETE":
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "del req !!}`))
+			w.Write([]byte(`{"message": "del req !!"}`))
 		default:
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "Not Found !!}`))
+			w.Write([]byte(`{"message": "Not Found !!"}`))
 	}
 
 
